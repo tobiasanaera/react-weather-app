819d014ad4dcccf49dd29fe5c302dac32bcd3322
tobiasanaera <131626785+tobiasanaera@users.noreply.github.com>
2023-09-28 22:54:56 +1300

    adding github repo and netlify URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Regina Thomson and is{" "}
+        <a
+          href="https://github.com/tobiasanaera/react-weather-app"
+          target="_blank"
+        >
+          Open-sourced on Github
+        </a>{" "}
+        and{" "}
+        <a href="https://master--frolicking-meringue-baf4a9.netlify.app/">
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
