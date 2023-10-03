66e120d780597edf164e13634e1ec2b67afd5d2a
tobiasanaera <131626785+tobiasanaera@users.noreply.github.com>
2023-10-03 22:07:35 +1300

    Creating weather App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Regina Thomson and is{" "}
-        <a
-          href="https://github.com/tobiasanaera/react-weather-app"
-          target="_blank"
-        >
-          Open-sourced on Github
-        </a>{" "}
-        and{" "}
-        <a href="https://master--frolicking-meringue-baf4a9.netlify.app/">
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Regina Thomson and is{" "}
+          <a
+            href="https://github.com/tobiasanaera/react-weather-app"
+            target="_blank"
+          >
+            Open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a href="https://master--frolicking-meringue-baf4a9.netlify.app/">
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
