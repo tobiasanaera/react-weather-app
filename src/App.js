7600962eba221491098a4bff114a395256093b9e
tobiasanaera <131626785+tobiasanaera@users.noreply.github.com>
2023-10-07 20:51:19 +1300

    added new files

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
+        <h1>Weather App</h1>
         <Weather />
         <footer>
           This project was coded by Regina Thomson and is{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
